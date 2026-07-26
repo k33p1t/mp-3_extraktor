6bb48b7c790110c0ece20ff567e40d361a507adb
k33p1t <102559802+k33p1t@users.noreply.github.com>
2026-07-26 01:07:40 -0700

    Create installation.bash

---
+++ installation.bash
@@ -0,0 +1,5 @@
+git clone https://github.com/yourusername/HoloAudio.git
+cd HoloAudio
+python -m venv venv
+source venv/bin/activate        # Windows: venv\Scripts\activate
+pip install -r requirements.txt
